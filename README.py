d9f5ab802bd88cf073e44868190bcc9896f7fab2
arularasi <arularasics@gmail.com>
2014-01-27 17:20:07 +0530

    Create README.py

---
+++ README.py
@@ -0,0 +1,14 @@
+airline-reseration
+==================
+for I in range(5):
+ for j in range(I,5):
+  print (" m ",end=' n')
+  fork in range(I):
+  Print("*",end=' nn')
+  Print()
+  for I in range(5):
+   for j in rangeI):
+    print" ",end =' '
+    For k inrangeI,5):
+    Print("*",end' n')
+    Print()
