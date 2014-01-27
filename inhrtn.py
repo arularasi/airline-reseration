90a5d46fbeb29ee6c1b01d83ddc073dd16ad9e5e
arularasi <arularasics@gmail.com>
2014-01-27 18:10:13 +0530

    Create inhrtn.py

---
+++ inhrtn.py
@@ -0,0 +1,15 @@
+class student:
+(s) name="Raul"
+(s)mark=90
+(s)definitely displayself):
+(s)(s)print("std name is0}\n studentmark is{1}",format(self.name,self.mark)
+ class sports(student):
+ (s)game="cricket"
+ (s)rank2
+ (s)defense play(self):
+ (s)(s)print("play game",self.game)
+ (s)(s)print("rank",self.rank)
+ S=sports()
+ S.display()
+ s,.play()
+ 
