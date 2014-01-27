7f830de16ae9ab8680335828a9d531e8ff06ad04
arularasi <arularasics@gmail.com>
2014-01-27 18:02:50 +0530

    Create mtrfun.py

---
+++ mtrfun.py
@@ -0,0 +1,15 @@
+def multiple(xy):
+C=0
+for I in range (3):
+ for j in range(3):
+  for k in range(3):
+   c+=x[I[j]*y[j][I]
+   print" ",c,end=' ')
+   Print()
+    a[[1,3,4],[5,6,3],[3,5,6]]
+     b=[[2,5,7],[8,6,7],[9,3,6]]
+      
+      mul(a,b)
+      
+      
+      
